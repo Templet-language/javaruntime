d495c9700e79a10da0db45f5d2810e70a6c8f985
Екатерина Скорюпина <e.skoryupina.develop@ya.ru>
2015-09-15 16:07:02 +0400

    Добавлен пример с каналами, процессами.

--- samples/templet/templet.java
+++ samples/templet/templet.java
@@ -13,3 +13,48 @@
 /*  See the License for the specific language governing permissions and     */
 /*  limitations under the License.                                          */
 /*--------------------------------------------------------------------------*/
+/*
+using namespace TEMPLET_DBG;
+
+double x;
+double r1,r2;
+
+engine e;
+chan cc1,cc2;
+proc pp,p1,p2;
+
+void c1(chan*c, proc*)
+{
+	r1=sin(x)*sin(x);
+	send(&e,c,&pp);
+}
+
+void c2(chan*c, proc*)
+{
+	r2=cos(x)*cos(x);
+	send(&e,c,&pp);
+}
+
+void p(chan*, proc*p)
+{
+	if(access(&cc1,p) && access(&cc2,p)){
+		cout<<r1+r2;
+	}
+}
+
+int main(int argc, char*argv[])
+{
+	pp.recv=p;
+	p1.recv=c1;
+	p2.recv=c2;
+
+	x=0.333;
+
+	send(&e,&cc1,&p1);
+	send(&e,&cc2,&p2);
+
+	run(&e, 2);
+
+	return 0;
+}
+*/
