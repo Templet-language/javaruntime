4d6edc0d0a9c31d543b65b1b43d1b9079a3407f9
Екатерина Скорюпина <e.skoryupina.develop@ya.ru>
2015-10-18 23:13:22 +0400

    Переведен пакет dbg; Незавершен метод run
    TODO: Изучить аналог auto в java, поскольку ведется работа с ним в методе run

--- lib/dbg/tet.java
+++ lib/dbg/tet.java
@@ -14,49 +14,104 @@
 /*  limitations under the License.                                          */
 /*--------------------------------------------------------------------------*/
 
-/* initial C++ runtime
+/* initial C++ runtime*/
 
-struct engine;
-struct proc;
-struct chan;
+package dbg;
 
+import java.util.ArrayList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.Condition;
+import java.util.Queue;
+
+public class tet {
+
+/*
 struct engine{
 	std::vector<chan*> ready;
 };
-
-struct proc{
-	void(*recv)(chan*, proc*);
-};
-
-struct chan{
-	proc*p;
-	bool sending;
-};
-
-inline void duration(engine*e, double t){}
-
-inline void send(engine*e, chan*c, proc*p)
-{
-	if (c->sending) return;
-	c->sending = true;
-	c->p = p;
-	e->ready.push_back(c);
-}
-
-inline bool access(chan*c, proc*p)
-{
-	return c->p == p && !c->sending;
-}
-
-inline void run(engine*e, int n = 1)
-{
-	size_t rsize;
-	while (rsize = e->ready.size()){
-		int n = rand() % rsize;	auto it = e->ready.begin() + n;
-		chan*c = *it;	e->ready.erase(it); c->sending = false;
-		c->p->recv(c, c->p);
-	}
-}
-
-inline void stat(engine*e, double&T1, double&Tp, int&Pmax, double&Smax, int P, double&Sp){}
 */
+
+    class engine {
+        private ArrayList<chan> ready;
+    }
+
+/*
+struct proc{
+    void(*recv)(chan*, proc*);
+};*/
+
+    class proc {
+        public abstract void recv(chan chan, proc proc);
+    }
+
+/*
+struct chan{
+    proc*p;
+    bool sending;
+};*/
+
+    class chan {
+        private proc p;
+        private boolean sending;
+    }
+
+/*inline void duration(engine*e, double t){}*/
+
+    public void duration(engine e, double t) {
+    }
+
+/*
+inline void send(engine*e, chan*c, proc*p) {
+    if (c -> sending) return;
+    c -> sending = true;
+    c -> p = p;
+    e -> ready.push_back(c);
+}*/
+
+    public void send(engine e, chan c, proc p) {
+        if (c.sending) {
+            return;
+        }
+        c.sending = true;
+        c.p = p;
+        e.ready.add(c);
+    }
+/*
+inline bool access(chan*c, proc*p) {
+    return c -> p == p && !c -> sending;
+}*/
+
+    public boolean access(chan c, proc p) {
+        return c.p == p && !c.sending;
+    }
+
+/*inline void run(engine*e, int n=1) {
+    size_t rsize;
+    while (rsize = e -> ready.size()) {
+        int n = rand() % rsize;
+        auto it = e -> ready.begin() + n;
+        chan * c =*it;
+        e -> ready.erase(it);
+        c -> sending = false;
+        c -> p -> recv(c, c -> p);
+    }
+}*/
+
+    public void run(engine e) {
+        final int RAND_MAX = 32767;
+        long rsize = 0;
+        while (rsize == e.ready.size()) {
+            int n = (int) ((Math.random() * RAND_MAX) % rsize);
+            //взять первый элемент  e.ready...
+
+        }
+
+    }
+
+
+/*inline void stat(engine*e, double&T1, double&Tp, int&Pmax, double&Smax, int P, double&Sp) {
+}*/
+
+    void stat(engine e, double T1, double Tp, int Pmax, double Smax, int P, double Sp) {
+    }
+}
