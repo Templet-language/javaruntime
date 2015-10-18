f344967285347fa3b35c32e1848455872905438c
Екатерина Скорюпина <e.skoryupina.develop@ya.ru>
2015-10-18 22:36:52 +0400

    Переведен пакет par, версия 1;
    Сложности возникали при использовании мьютексов, требуется проверка на правильное объявление и работу с ними.
    inline модификатор отсутствует в java, оптимизация выполняется компилятором JVM.
    Не использованы возможности класса Condition (требуют дополнительного изучения)

--- lib/par/tet.java
+++ lib/par/tet.java
@@ -16,75 +16,148 @@
 
 /* initial C++ runtime
 
-struct engine;
-struct proc;
-struct chan;
+package par;
 
-struct engine{
-	volatile int active;
-	std::mutex mtx;
-	std::condition_variable cv;
-	std::queue<chan*> ready;
-};
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.Condition;
+import java.util.Queue;
 
+public class tet {
+    /*
+    struct engine{
+        volatile int active;
+        std::mutex mtx;
+        std::condition_variable cv;
+        std::queue<chan*> ready;
+    }
+     */
+class engine {
+	private int active;
+	private Lock mtx;
+	private Condition cv;
+	private Queue<chan> ready;
+}
+/*
 struct proc{
-	std::mutex mtx;
-	void(*recv)(chan*, proc*);
+    std::mutex mtx;
+    void(*recv)(chan*, proc*);
 };
+*/
+abstract class proc{ //кажется это интерфейсы
+	private Lock mtx;
+	public abstract void recv (chan chan,proc proc);
+}
 
+/*
 struct chan{
-	proc*p;
-	bool sending;
+    proc*p;
+    bool sending;
 };
-
-inline void duration(engine*e, double t){}
-
-inline void send(engine*e, chan*c, proc*p)
-{
-	if (c->sending) return;
-	c->sending = true;	c->p = p;
-	std::unique_lock<std::mutex> lck(e->mtx);
-	e->ready.push(c);	e->cv.notify_one();
+*/
+class chan{
+	private proc p;
+	private boolean sending;
 }
 
-inline bool access(chan*c, proc*p)
-{
-	return c->p == p && !c->sending;
+    /*
+    inline void duration(engine*e, double t){}
+    */
+
+	public void duration (engine e, double t){}
+
+	/*
+    inline void send(engine*e, chan*c, proc*p)
+    {
+    if (c->sending) return;
+    c->sending = true;	c->p = p;
+    std::unique_lock<std::mutex> lck(e->mtx);
+    e->ready.push(c);	e->cv.notify_one();
 }
-
-inline void tfunc(engine*e)
-{
-	chan*c; proc*p;
-
-	for (;;){
+     */
+	public void send(engine e,chan c, proc p)
+	{
+		if (c.sending)
 		{
-			std::unique_lock<std::mutex> lck(e->mtx);
-			while (e->ready.empty()){
-				e->active--;
-				if (!e->active){ e->cv.notify_one(); return; }
-				e->cv.wait(lck);
-				e->active++;
-			}
-			c = e->ready.front();
-			e->ready.pop();
+			return;
 		}
-		p = c->p;
+		c.sending = true;
+		c.p=p;
+		//start of the critical section
+		e.mtx.tryLock();
+		e.ready.add(c); //Нужно ли делать unlock()?
+		e.cv.notify();
+		//end of the critical section
+	}
+/*
+    inline bool access(chan*c, proc*p)
+    {
+        return c->p == p && !c->sending;
+    }
+*/
+
+	public boolean access(chan c, proc p)
+	{
+		return c.p == p && !c.sending;
+	}
+	/*
+        inline void tfunc(engine*e)
+        {
+            chan*c; proc*p;
+
+            for (;;){
+                {
+                    std::unique_lock<std::mutex> lck(e->mtx);
+                    while (e->ready.empty()){
+                        e->active--;
+                        if (!e->active){ e->cv.notify_one(); return; }
+                        e->cv.wait(lck);
+                        e->active++;
+                    }
+                    c = e->ready.front();
+                    e->ready.pop();
+                }
+                p = c->p;
+                {
+                    std::unique_lock<std::mutex> lck(p->mtx);
+                    c->sending = false;
+                    p->recv(c, p);
+                }
+            }
+        }
+    */
+	public void tfunc (engine e)
+	{
+		chan c;
+		proc p;
+
+		try {
+
+			for (; ; ) {
+				//start of the critical section
+				e.mtx.tryLock();
+				while (e.ready.isEmpty()) {
+					e.active--;
+					if (!(e.active == 0)) {
+						e.cv.notify();
+						return;
+					}
+					e.mtx.wait();
+					e.active++; //Нужно ли делать unlock()?
+					//end of the critical section
+				}
+				c = e.ready.poll(); //Retrieves and removes the head of this queue, or returns null if this queue is empty.
+
+				p = c.p;
+				//start of the critical section
+				p.mtx.tryLock();
+				c.sending = false;
+				p.recv(c, p);
+				//end of the critical section
+			}
+		}
+		catch (InterruptedException interExc)
 		{
-			std::unique_lock<std::mutex> lck(p->mtx);
-			c->sending = false;
-			p->recv(c, p);
+			interExc.getMessage();
 		}
 	}
 }
-
-inline void run(engine*e, int n = 1)
-{
-	std::vector<std::thread> threads(n);
-	e->active = n;
-	for (int i = 0; i<n; i++) threads[i] = std::thread(tfunc, e);
-	for (auto& th : threads) th.join();
-}
-
-inline void stat(engine*e, double&T1, double&Tp, int&Pmax, double&Smax, int P, double&Sp){}
-
-*/
