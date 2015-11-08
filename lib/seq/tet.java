5a552442e4f71f975bd32af0602cac7216ec2e4b
skoryupina <e.skoryupina.develop@ya.ru>
2015-11-09 01:13:31 +0400

    Переведен пакет seq, версия 1;

--- lib/seq/tet.java
+++ lib/seq/tet.java
@@ -14,38 +14,66 @@
 /*  limitations under the License.                                          */
 /*--------------------------------------------------------------------------*/
 
-/* initial C++ runtime
-struct engine;
-struct proc;
-struct chan;
+/* initial C++ runtime*/
 
+/*
 struct engine{
 	std::queue<chan*> ready;
 };
+*/
 
+	class engine {
+		private Queue<chan> ready;
+	}
+/*
 struct proc{
 	void(*recv)(chan*, proc*);
-};
+};*/
 
+	abstract class proc {
+		public abstract void recv(chan chan, proc proc);
+	}
+/*
 struct chan{
 	proc*p;
 	bool sending;
-};
+};*/
 
-inline void duration(engine*e, double t){}
+	class chan {
+		private proc p;
+		private boolean sending;
+	}
+/*
+inline void duration(engine*e, double t){}*/
 
+	public void duration(engine e, double t) {
+	}
+/*
 inline void send(engine*e, chan*c, proc*p)
 {
 	if (c->sending) return;
 	c->sending = true;	c->p = p;
 	e->ready.push(c);
-}
+}*/
 
+	public void send(engine e, chan c, proc p) {
+		if (c.sending) {
+			return;
+		}
+		c.sending = true;
+		c.p = p;
+		e.ready.add(c);
+	}
+/*
 inline bool access(chan*c, proc*p)
 {
 	return c->p == p && !c->sending;
-}
+}*/
 
+	public boolean access(chan c, proc p) {
+		return c.p == p && !c.sending;
+	}
+/*
 inline void run(engine*e, int n = 1)
 {
 	while (!e->ready.empty()){
@@ -53,7 +81,20 @@ inline void run(engine*e, int n = 1)
 		c->sending = false;
 		c->p->recv(c, c->p);
 	}
-}
+}*/
 
+	public void run(engine e) {
+			final int RAND_MAX = 32767;
+			long rsize = 0;
+			while (e.ready.size()!=0) {
+				chan c = e.ready.poll(); // получаем первый элемент и удаляем его из очереди
+				c.sending=false;
+				c.p.recv(c,c.p);
+			}
+		}
+/*
 inline void stat(engine*e, double&T1, double&Tp, int&Pmax, double&Smax, int P, double&Sp){}
 */
+	void stat(engine e, double T1, double Tp, int Pmax, double Smax, int P, double Sp) {
+		}
+}
